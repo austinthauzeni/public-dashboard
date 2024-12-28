cb71f6c5b429484c665b75a5afbded3119d0d0e9
austinthauzeni1 <austinthauzeni1@gmail.com>
2024-12-28 17:27:48 +0200

    feat: added different pages

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
+import { createInvoice } from '@/app/lib/actions';
 import {
   CheckIcon,
   ClockIcon,
@@ -7,10 +10,13 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+
+
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
